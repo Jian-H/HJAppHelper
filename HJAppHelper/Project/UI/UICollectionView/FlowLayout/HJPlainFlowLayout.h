f0751380fc289a1c6e71b2bfe1f8f6cfbe03ca0c
huangjian <huangjian@182.92.149.44>
2016-06-28 08:22:04 +0800

    提交一个collectionview 的header悬浮模式

---
+++ HJAppHelper/Project/UI/UICollectionView/FlowLayout/HJPlainFlowLayout.h
@@ -0,0 +1,15 @@
+//
+//  HJPlainFlowLayout.h
+//  HJAppHelper
+//
+//  Created by huangjian on 16/6/28.
+//  Copyright © 2016年 huangjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HJPlainFlowLayout : UICollectionViewFlowLayout
+
+@property (nonatomic, assign) CGFloat naviHeight;//默认0
+
+@end
